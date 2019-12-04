2f20cd8a05d2af16f3ee32ad5b46579744285e97
linhletd <42126424+linhletd@users.noreply.github.com>
2019-12-04 23:07:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-
+var helmet      = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
-
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
